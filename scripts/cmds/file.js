fd20d30410e5982c49c8c8bd2e5d8a8358189373
ChanelleKoum <160431617+ChanelleKoum@users.noreply.github.com>
2024-10-12 07:20:20 +0100

    Add files via upload

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,4 +1,4 @@
- const fs = require('fs');
+const fs = require('fs');
 
 module.exports = {
   config: {
@@ -14,19 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100084606198768"];
+    const permission = ["100081169801569","61550511050291","100090586851243","100054965226399","100074714075081","100078189592811","100086568053014","100094476012105","61551400375931"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("🔞 ce cmd est interdit aux enfants de ton âge 🙃", event.threadID, event.messageID);
+      return api.sendMessage("- Bitch, Only my Boss Mr ʚɸɞ Dominre ʚɸɞ can use this👿🖕🏾", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("le fichier ❓.", event.threadID, event.messageID);
+      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`𝐃𝐞𝐬𝐨𝐥𝐞́ 𝐛𝐨𝐬𝐬 𝐜𝐞𝐭𝐭𝐞 𝐜𝐦𝐝 𝐧'𝐞𝐱𝐢𝐬𝐭𝐞 𝐩𝐚𝐬 𝐝𝐚𝐧𝐬 𝐦𝐞𝐬 𝐜𝐦𝐝𝐬 [✖]: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
