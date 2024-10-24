e7d3ec9e87e6ed37816997e784935ea4b2c8b446
ChanelleKoum <160431617+ChanelleKoum@users.noreply.github.com>
2024-10-24 05:06:54 +0100

    Update upt.js

--- scripts/cmds/upt.js
+++ scripts/cmds/upt.js
@@ -1,9 +1,9 @@
- module.exports = {
+module.exports = {
   config: {
     name: "upt",
     aliases: ["up"],
     version: "1.0",
-    author: "Ronald",
+    author: "Dominre",
     role: 2,
     shortDescription: {
       en: "Displays the total number of users of the bot and check uptime "
@@ -28,11 +28,12 @@ Math.floor(uptime / (3600 * 24));
       const minutes = Math.floor((uptime % 3600) / 60);
       const seconds = Math.floor(uptime % 60);
       
-      const uptimeString = `${days} : ${hours} : ${minutes} : ${seconds}`;
+      const uptimeString = `${days}𝘿𝙖𝙮𝙨 : ${hours}𝙃𝙤𝙪𝙧𝙨 : ${minutes}𝙈𝙞𝙣𝙪𝙩𝙚𝙨 : ${seconds}𝙎𝙚𝙘𝙤𝙣𝙙𝙨`;
       
-      api.sendMessage(`[⌛] 𝙏𝙞𝙢𝙚\n\n✪➩ ${uptimeString}\n
-[👨🏽‍🦱] 𝙉𝙪𝙢𝙗𝙚𝙧 𝙛𝙤𝙧 𝙪𝙨𝙚𝙧𝙨\n\n✪➩ ${allUsers.length}\n
-[💬] 𝙉𝙪𝙢𝙗𝙚𝙧 𝙛𝙤𝙧 𝙙𝙞𝙨𝙘𝙪𝙨𝙨𝙞𝙤𝙣\n\n✪➩ ${allThreads.length}`, event.threadID);
+      api.sendMessage(`[⚙️] 𝗨𝗽𝘁𝗶𝗺𝗲 𝗦𝗲𝘁𝘁𝗶𝗻𝗴\n\n✪➩ ${uptimeString}\n
+[👨🏽‍💻] 𝗡𝗼𝗺𝗯𝗿𝗲 𝗗'𝘂𝘁𝗶𝗹𝗶𝘀𝗮𝘁𝗲𝘂𝗿𝘀\n\n✪➩ ${allUsers.length}\n
+[📬] 𝗡𝗼𝗺𝗯𝗿𝗲 𝗗𝗲𝘀 𝗚𝗿𝗼𝘂𝗽𝗲𝘀 𝗜𝗻𝘁𝗲́𝗴𝗿𝗲́𝘀 🍷\n\n✪➩ ${allThreads.length}\n
+[🥂]Slogan du jour : 𝐋𝐚 𝐯𝐢𝐞, 𝐜𝐞 𝐧'𝐞𝐬𝐭 𝐩𝐚𝐬 𝐝'𝐚𝐭𝐭𝐞𝐧𝐝𝐫𝐞 𝐪𝐮𝐞 𝐥𝐞𝐬 𝐨𝐫𝐚𝐠𝐞𝐬 𝐩𝐚𝐬𝐬𝐞𝐧𝐭, 𝕔'𝕖𝕤𝕥 𝕕'𝕒𝕡𝕡𝕣𝕖𝕟𝕕𝕣𝕖 𝕔𝕠𝕞𝕞𝕖𝕟𝕥 𝕕𝕒𝕟𝕤𝕖𝕣 𝘀𝗼𝘂𝘀 𝗹𝗮 𝗽𝗹𝘂𝗶𝗲 💫❤😁`, event.threadID);
     } catch (error) {
       console.error(error);
       api.sendMessage("An error occurred while retrieving data.", event.threadID);
